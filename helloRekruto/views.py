5e5aeb58419ea543e6237045c00e2ae29c6948d5
nktph <nkt.ph.coop@gmail.com>
2022-03-11 17:33:30 +0300

    correcting name and message field

--- helloRekruto/views.py
+++ helloRekruto/views.py
@@ -6,8 +6,10 @@ def helloRekruto(request):
     name = request.GET.get('name')
     message = request.GET.get('message')
 
-    if not name and not message:
+    if not name:
         name="Rekruto"
+
+    if not message:
         message="Давай дружить"
 
     return render(request, 'hello.html',{'name':name, 'message':message})
